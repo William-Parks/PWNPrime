00162c7c532fab3f38b4b213e454dd06ac2b46a7
William Parks <william.parks2@gmail.com>
2021-03-04 12:49:34 -0600

    Updates 'n Stuff

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "
 :::::::::  :::       ::: ::::    ::: :::::::::  :::::::::  ::::::::::: ::::    ::::  :::::::::: 
-:+:    :+: :+:       :+: :+:+:   :+: :+:    :+: :+:    :+:     :+:     +:+:+: :+:+:+ :+:        
+
 +:+    +:+ +:+       +:+ :+:+:+  +:+ +:+    +:+ +:+    +:+     +:+     +:+ +:+:+ +:+ +:+        
 +#++:++#+  +#+  +:+  +#+ +#+ +:+ +#+ +#++:++#+  +#++:++#:      +#+     +#+  +:+  +#+ +#++:++#   
 +#+        +#+ +#+#+ +#+ +#+  +#+#+# +#+        +#+    +#+     +#+     +#+       +#+ +#+        
@@ -25,7 +25,9 @@ sudo apt -y update && sudo apt -y upgrade && sudo apt -y autoremove && sudo apt 
 
 ## Pimp My Kali
 git clone https://github.com/Dewalt-arch/pimpmykali.git ~/Desktop/tools/pimpit/pimpmykali
+echo "Starting PimpMyKali"
 
+~/Desktop/tools/pimpit/pimpmykali/pimpmykali.sh
 ## Create Directories
 mkdir -p ~/Desktop/tools/
 cd ~/Desktop/tools/
@@ -42,8 +44,8 @@ cd ../../../../..
 ## Web Pentesting
 echo "Web Pentesting Tools Installing..."
 echo "Rust Scan"
-##	RustScan
-##		Downloads the amd64.deb package and runs `sudo apt install [PKGNAME].deb`
+##      RustScan
+##              Downloads the amd64.deb package and runs `sudo apt install [PKGNAME].deb`
 wget https://github.com/RustScan/RustScan/releases/download/2.0.1/rustscan_2.0.1_amd64.deb -O ~/Desktop/tools/web/rustscan_2.0.1_amd64.deb
 sudo apt install -y ~/Desktop/tools/web/rustscan_2.0.1_amd64.deb
 rm -f ~/Desktop/tools/web/rustscan_2.0.1_amd64.deb
@@ -52,8 +54,8 @@ echo "Done!"
 
 echo ""
 echo "feroxbuster"
-##	feroxbuster
-##		Downloads the amd64.deb package and runs `sudo apt install [PKGNAME].deb`
+##      feroxbuster
+##              Downloads the amd64.deb package and runs `sudo apt install [PKGNAME].deb`
 wget https://github.com/epi052/feroxbuster/releases/download/v2.0.0/feroxbuster_amd64.deb.zip -O ~/Desktop/tools/web/feroxbuster_amd64.deb.zip
 unzip ~/Desktop/tools/web/feroxbuster_amd64.deb.zip -d ~/Desktop/tools/web/
 sudo apt install -y ~/Desktop/tools/web/feroxbuster_2.0.0_amd64.deb
@@ -63,7 +65,7 @@ echo "Done!"
 
 echo ""
 echo "name-that-hash"
-##	name-that-hash
+##      name-that-hash
 if pip3 install name-that-hash; then 
 echo "Installing using pip3"
 echo "Successfully Installed"
@@ -74,7 +76,7 @@ echo "Done!"
 
 echo ""
 echo "ffuf"
-##	ffuf
+##      ffuf
 apt install ffuf
 echo "Done!"
 
@@ -82,13 +84,13 @@ echo ""
 echo "Wordlists Installing Now..."
 ## Wordlists
 echo "SecLists"
-##	SecLists
+##      SecLists
 git clone https://github.com/danielmiessler/SecLists.git ~/Desktop/tools/wordlists/SecLists
 echo "Done!"
 
 echo ""
 echo "Crackstation Wordlists"
-##	Crack station wordlists
+##      Crack station wordlists
 wget https://crackstation.net/files/crackstation-human-only.txt.gz -O ~/Desktop/tools/wordlists/crackstation-human-only.txt.gz
 gzip ~/Desktop/tools/wordlists/crackstation-human-only.txt.gz
 echo "Done!"
@@ -97,7 +99,7 @@ echo ""
 echo "Enumeration Tools Installing now..."
 ## Enumeration Tools
 echo "enum4linux-ng"
-##	enum4linux-ng
+##      enum4linux-ng
 git clone https://github.com/cddmp/enum4linux-ng.git ~/Desktop/tools/enumeration/enum4linux-ng
 echo "Done!"
 
@@ -105,7 +107,7 @@ echo ""
 echo "Terminal Tools Installing Now..."
 ## Terminal and Editing
 echo "tmux redesign"
-##	Oh My Tmux
+##      Oh My Tmux
 git clone https://github.com/gpakosz/.tmux.git ~/Desktop/tools/terminal/tmux
 cp ~/Desktop/tools/terminal/tmux/.tmux.conf ~
 echo "Done!"
@@ -113,15 +115,15 @@ echo "Done!"
 
 echo ""
 echo "Emacs"
-##	Emacs install
-##		Ensure emacs is installed
+##      Emacs install
+##              Ensure emacs is installed
 apt install emacs
 echo "Done!"
 
 #echo ""
 #echo "Doom Emacs"
-##	Doom Emacs
-##		Make optional
+##      Doom Emacs
+##              Make optional
 #add-apt-repository ppa:kelleyk/emacs
 #apt-get update
 #apt-get install emacs26
@@ -135,24 +137,32 @@ echo "Done!"
 #echo "Done!"
 
 echo ""
-echo "Nvim --- FIXING THIS NOW"
-##	Nvim
-sudo apt install gperf luajit luarocks libuv1-dev libluajit-5.1-dev libunibilium-dev libmsgpack-dev libtermkey-dev libvterm-dev libutf8proc-dev
-sudo luarocks build mpack
-sudo luarocks build lpeg
-sudo luarocks build inspect
+echo "Nvim"
+##      Nvim
+sudo apt install neovim
 cd ~/Desktop/tools/terminal
 mkdir nvim
-wget https://github.com/neovim/neovim/releases/latest/download/nvim.appimage -O ~/Desktop/tools/terminal/nvim/nvim.appimage
-chmod u+x ~/Desktop/tools/terminal/nvim/nvim.appimage
-~/Desktop/tools/terminal/nvim/nvim.appimage
+touch nvim_Installed.txt
+echo "Installing Visual Packages Now..."
+
+echo ""
+echo "vim (vim-airline) Package"
+mkdir -p ~/.vim/pack/vendor/start
+git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/vendor/start/
+mv ~/.vim/pack/vendor/start/vim-airline ~/.vim/pack/vendor/start/airline
+touch ~/.vim/pack/vendor/start/setup.txt
+echo "To fully install the airline package (or any package), just clone a github repo to here and run the ':packadd' followed by the package name. Example: ':packadd airline'. After that is done, open up vim and run the command ':helptags ALL' to install all of the packages that you have enabled and their respective commands." > ~/.vim/pack/vendor/start/setup.txt
 echo "Done!"
+
+echo ""
+echo "lightline vim"
+##      INCLUDE LIGHTLINE VIM INSTALL HERE
 
 echo ""
 echo "SMB Share Tools Installing Now..."
 ## SMB Share Testing
 echo "CrackMapExec"
-##	Crack Map Exec
+##      Crack Map Exec
 git clone https://github.com/byt3bl33d3r/CrackMapExec.git ~/Desktop/tools/smb/crackmapexec
 echo "Done!"
 
@@ -160,7 +170,7 @@ echo ""
 echo "Windows Tools Installing Now..."
 ## Windows Tools
 echo "Bloodhound"
-##	Bloodhound
+##      Bloodhound
 echo "Installing unzip for Bloodhound"
 sudo apt install unzip
 wget https://github.com/BloodHoundAD/BloodHound/releases/download/4.0.2/BloodHound-linux-x64.zip -O ~/Desktop/tools/Windows/Bloodhound-linux-x64.zip
@@ -172,8 +182,8 @@ echo "Lastly, Notetaking Software Installing..."
 
 ## Notetaking Software
 echo "Joplin"
-##	Joplin
-##		Auto Install of Joplin
+##      Joplin
+##              Auto Install of Joplin
 
 wget -O ~/joplin_install.sh https://raw.githubusercontent.com/laurent22/joplin/dev/Joplin_install_and_update.sh
 chmod +x ~/joplin_install.sh
@@ -182,7 +192,7 @@ rm -f ~/joplin_install.sh
 sudo ln -s ~/.joplin/Joplin.AppImage /usr/bin/joplin
 if [ "$EUID" = 0 ] 
 then
-	echo "alias jop='joplin --no-sandbox'" >> .zshrc
+        echo "alias jop='joplin --no-sandbox'" >> .zshrc
 fi
 echo "Done!"
 
@@ -197,11 +207,3 @@ echo ""
 
 echo "Waiting for 10 seconds..."
 sleep 5
-
-echo "Starting PimpMyKali"
-
-~/Desktop/tools/pimpit/pimpmykali/pimpmykali.sh
-
-
-
-echo ""
